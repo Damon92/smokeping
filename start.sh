a0a399cfd8a7b58373a9d00d1cb64ebbdea4a3c6
Damon92 <874467785@qq.com>
2021-01-22 12:29:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #/bin/sh
 systemctl start prometheus
 systemctl start pushgateway
+systemctl start crond
 service grafana-server start
 /usr/local/smokeping/bin/smokeping
+echo "* * * * * python /usr/local/smokeping/collection_to_prometheus.py" >> /var/spool/cron/root
+mv /etc/localtime /etc/localtime.bak
+cp -rf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
